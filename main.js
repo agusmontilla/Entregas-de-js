5edabf0b83bd33e8969a9c76e8e5dc2737d41db3
agusmontilla <agustinmontilla16@gmail.com>
2023-04-04 20:22:12 -0300

    subida de la segunda entrega

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-
-alert("Bienvenido a la tienda de bebidas")
+function menu(){
+alert("Bienvenido a la tienda de juegos online")
 
 let usuario = prompt("Ingrese su nombre de usuario:")
 const passwordUsuario = 1234
@@ -8,60 +8,164 @@ let password = prompt("Ingrese su contraseña:")
 for(let i=0 ; 1<=3; i++){
 	if(password == passwordUsuario){
 		alert("contraseña correcta")
+		comprar()
 		break
 	}else{
 		password=prompt("Contraseña incorrecta, vuelva a intentarlo:")
 	}
+
+}
 }
 
 
-let edadUsuario=prompt("Ingrese su edad:")
-for(let i=0 ; 1<=3; i++){
-	if(edadUsuario>=18){
-		alert("Usted es mayor de edad, puede ingresar")
-		break
-	}else{
-		alert("Usted es menor de edad, no puede ingresar")
+
+class Producto{
+	constructor(id, nombre, genero, precio){
+		this.id=id 
+		this.nombre=nombre	
+		this.genero=genero
+		this.precio=precio
 	}
 }
 
-let bebida = prompt("Ingrese la bebida para saber el precio:(Pon exit para salir) ")
-while(bebida != "exit"){
-		switch(bebida){
-		case "agua":
-			alert("El valor del agua es $250")
-			break
-		case "levite":
-			alert("El valor de la levite es $350 ")	
-			break
-		case "coca cola":
-			alert("El valor de la coca cola es $450")
-			break
-		case "sprite":
-			alert("El valor de la sprite es $450")
-			break
-		case "cerveza":
-			alert("el valor de la cerveza es $600")
-			break
-		default:
-			alert("Producto sin stock")
-			break		
+const p1 = new Producto(1,'Fifa 23', 'Deporte', 20000)
+const p2=new Producto(2, 'The last of us', 'Accion', 25000)
+const p3 =new Producto(3, 'Resident Evil 4', 'Terror', 29000)
+const p4 =new Producto(4, 'Gran Turismo 7', 'Carreras', 22000)
+
+const productos = [p1,p2,p3,p4]
+
+let carrito =[]
+
+
+
+
+function comprar(){
+	let comprarJuego = parseInt(prompt("Ingrese una opcion: 0)Fifa 23  1)The last of us  2)Resident Evil 4  3)Gran Turismo 7  4)Volver al menú:"))
+	console.log(comprarJuego)
+	let juego= productos[comprarJuego]
+	console.log(juego)
+	let precio=0
+	let cantidad=0
+
+	switch(comprarJuego){
+		 case 0:
+		 	cantidad = cantidad + parseInt(prompt("Ingrese la cantidad que desea comprar:")) 
+		 	precio=20000
+		 	carrito.push({juego, cantidad, precio})
+		 	console.log(carrito)
+
+		 	let seguir1 = prompt("Ingrese 1) Si quiere seguir comprando 2)Si quiere ir al detalle de compra") 
+		 	switch(seguir1){
+		 	case "1":
+                comprar()
+                break
+            case "2":
+                alert("A continuacion tiene el detalle de su compra")
+                calcularTotalCompra()
+		 	}
+		 	return
+
+
+		 case 1:
+		 	cantidad = cantidad + parseInt(prompt("Ingrese la cantidad que desea comprar:"))
+		 	precio = 25000
+		 	carrito.push({juego, cantidad, precio})
+		 	console.log(carrito)
+
+		 	let seguir2 = prompt("Ingrese 1) Si quiere seguir comprando 2)Si quiere ir al detalle de compra")
+		 	switch(seguir2){
+		 	case "1":
+                comprar()
+                break
+            case "2":
+                alert("A continuacion tiene el detalle de su compra")
+                calcularTotalCompra()
+		 	}
+		 	return
+
+		 case 2:
+		 	cantidad = cantidad +parseInt(prompt("Ingrese la cantidad que desea comprar:"))
+		 	precio = 29000
+		 	carrito.push({juego, cantidad, precio})
+		 	console.log(carrito)
+
+		 	let seguir3 = prompt("Ingrese 1) Si quiere seguir comprando 2)Si quiere ir al detalle de compra")
+		 	switch(seguir3){
+		 	case "1":
+                comprar()
+                break
+            case "2":
+                alert("A continuacion tiene el detalle de su compra")
+                calcularTotalCompra()
+		 	}
+		 	return
+
+		 case 3:
+		 	cantidad = cantidad + parseInt(prompt("Ingrese la cantidad que desea comprar:"))
+		 	precio = 22000
+		 	carrito.push({juego, cantidad, precio})
+		 	console.log(carrito)
+
+		 	let seguir4 = prompt("Ingrese 1) Si quiere seguir comprando 2)Si quiere ir al detalle de compra")
+		 	switch(seguir4){
+		 	case "1":
+                comprar()
+                break
+            case "2":
+                alert("A continuacion tiene el detalle de su compra")
+                calcularTotalCompra()
+		 	}
+		 	return
+
+		 case 4:
+		 	menu()
+
+		 default:
+		 	alert("Elija una opcion correcta")
+		 	comprar()
+
+
+            } 
+                
+            
 	}
-	bebida = prompt("Ingrese la bebida para saber el precio:(Pon exit para salir) ")
+
+
+
+
+
+
+
+function calcularTotalCompra(){
+
+carrito.forEach((carritoFinal) => {console.log(` producto: ${carritoFinal.juego}, cantidad: ${carritoFinal.cantidad}, precio: ${carritoFinal.precio} `) })
+
+
+const total= carrito.reduce((acumulador, elementos) => acumulador + elementos.precio * elementos.cantidad, 0)
+alert(`El total a pagar es: ${total}`)
+
+
+const salida = prompt("Ingrese 1)Para volver al menu principal  2)Para salir")
+if(salida== 1){
+	menu()
+
+}else if(salida==2){
+	salir()
 }
 
-let acumulador = 0;
-let cantidadProductos=prompt("¿Cuantos productos desea comprar?")
-	
-	for(let i=1; i<=cantidadProductos; i++){
-		let precio=parseInt(prompt("Ingrese el precio del producto N°"+i +" (Sin $)"));
-		acumulador = acumulador + precio
-	}
-	alert("el valor de su compra es $"+acumulador +" sin descuento")
 
-function calcularDescuento(total){
-	let totalConDescuento = total * 0.9;
-	alert("El total de la compra con el 10% de descuento incluido es $"+totalConDescuento)
 
-}
-calcularDescuento(acumulador)
+} 
+ 
+ function salir(){
+ 	alert("Gracias por su compra")
+ }
+
+
+ menu()
+
+
+  
+
+  
